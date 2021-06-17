c2c4906ddfd807b106d5194cc825195346fa84e1
TheSink <lucianedean@gmail.com>
2021-06-16 21:31:21 -0700

    test the download system

--- download.lua
+++ download.lua
@@ -1,6 +1,12 @@
 local scripts = {"main", "screen_debug", "download"}
 
+print('v2')
+
 for _, scriptName in pairs(scripts) do
-    fs.delete(scriptName..".lua")
-    shell.run("wget https://raw.githubusercontent.com/the-sink/elevator-controller/main/"..scriptName..".lua")
+    shell.run("wget https://raw.githubusercontent.com/the-sink/elevator-controller/main/"..scriptName.."-temp.lua")
+    if fs.exists(scriptName.."-temp.lua") then
+        fs.delete(scriptName..".lua")
+        fs.copy(scriptName.."-temp.lua", scriptName..".lua")
+        fs.delete(scriptName.."-temp.lua")
+    end
 end
