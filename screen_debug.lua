afe3c9bdcefe712f417bc16ae3789f8583478fdf
the-sink <lucianedean@gmail.com>
2021-06-16 21:44:28 -0700

    only display screen number

--- screen_debug.lua
+++ screen_debug.lua
@@ -5,7 +5,7 @@ for _, name in pairs(peripherals) do
         local monitor = peripheral.wrap(name)
         monitor.clear()
         monitor.setCursorPos(1, 1)
-        monitor.write(name)
+        monitor.write(string.sub(name, -1))
         print(name)
     end
 end
