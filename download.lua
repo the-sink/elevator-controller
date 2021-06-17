cf5a5497eed5244687b18e1063bc729074385a25
TheSink <lucianedean@gmail.com>
2021-06-16 21:25:03 -0700

    add script downloader

---
+++ download.lua
@@ -0,0 +1,6 @@
+local scripts = {"main", "screen_debug", "download"}
+
+for _, scriptName in pairs(scripts) do
+    fs.delete(scriptName..".lua")
+    shell.run("wget https://raw.githubusercontent.com/the-sink/elevator-controller/main"..scriptName..".lua")
+end
