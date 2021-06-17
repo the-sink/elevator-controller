895126a340959a01a8c2738945a60c07319a50ec
TheSink <lucianedean@gmail.com>
2021-06-16 21:38:25 -0700

    fix issue with download again

--- download.lua
+++ download.lua
@@ -1,9 +1,8 @@
 local scripts = {"main", "screen_debug", "download"}
 
-print('v2')
-
 for _, scriptName in pairs(scripts) do
-    shell.run("wget https://raw.githubusercontent.com/the-sink/elevator-controller/main/"..scriptName.."-temp.lua")
+    local data = http.get()
+    shell.run("wget https://raw.githubusercontent.com/the-sink/elevator-controller/main/"..scriptName..".lua "..scriptName.."-temp.lua")
     if fs.exists(scriptName.."-temp.lua") then
         fs.delete(scriptName..".lua")
         fs.copy(scriptName.."-temp.lua", scriptName..".lua")
