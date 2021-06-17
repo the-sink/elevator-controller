cbfe63e8134223d5b90ee2e387520478c3d8f0f3
TheSink <lucianedean@gmail.com>
2021-06-16 21:40:44 -0700

    remove accidental inclusion of http.get()

--- download.lua
+++ download.lua
@@ -1,7 +1,6 @@
 local scripts = {"main", "screen_debug", "download"}
 
 for _, scriptName in pairs(scripts) do
-    local data = http.get()
     shell.run("wget https://raw.githubusercontent.com/the-sink/elevator-controller/main/"..scriptName..".lua "..scriptName.."-temp.lua")
     if fs.exists(scriptName.."-temp.lua") then
         fs.delete(scriptName..".lua")
