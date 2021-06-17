f6e35a9dd1bde3c434989ab69fda5dd371bb657f
the-sink <lucianedean@gmail.com>
2021-06-16 21:49:45 -0700

    fix scale

--- screen_debug.lua
+++ screen_debug.lua
@@ -4,7 +4,7 @@ for _, name in pairs(peripherals) do
     if string.find(name, "monitor") then
         local monitor = peripheral.wrap(name)
         monitor.clear()
-        monitor.scale(1.5)
+        monitor.setTextScale(0.5)
         monitor.setCursorPos(1, 1)
         monitor.write(string.sub(name, -1))
         print(name)
